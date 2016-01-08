e27a7eeef3535b47d2738de71f0eddf0cfe1db24
Evgeny Bogdanov <evgeny.bogdanov@gmail.com>
2016-01-08 10:16:25 +0100

    add modal linked to tabs below

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -1,11 +1,39 @@
+let Transition = React.addons.CSSTransitionGroup;
+
 AppBody = React.createClass({
+  getDefaultProps() {
+    return {
+      tabs: ["Tab 1", "Tab 2", "Tab 3"]
+    }
+  },
+  getInitialState() {
+    return {
+      modal: false
+    }
+  },
+  ionModal(tab) {
+    this.setState({
+      modal: (
+        <IonModal>
+          <div className="h1 title">{tab}</div>
+          <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
+            <i className="icon ion-ios-close-empty"></i>
+          </button>
+        </IonModal>
+      )
+    })
+  },
   render() {
     return (
       <div className="ionic-body">
+        {this.state.modal ? <Backdrop /> : false}
+        <Transition transitionName="modal">
+          {this.state.modal}
+        </Transition>
         <div className="bar bar-header bar-light">
-          <ReactRouter.Link className="button button-icon icon ion-gear-a" to={"/settings"}></ReactRouter.Link>
+          <ReactRouter.Link className="button button-icon icon ion-ios-search-strong" to={"/settings"}></ReactRouter.Link>
           <ReactRouter.Link className="h1 title" to={"/"}>Clikque</ReactRouter.Link>
-          <ReactRouter.Link className="button button-icon icon ion-heart" to={"/other"}></ReactRouter.Link>
+          <ReactRouter.Link className="button button-icon icon ion-android-person" to={"/other"}></ReactRouter.Link>
         </div>
         <div className="view">
           <div className="scroll-content ionic-scroll">
@@ -13,6 +41,17 @@ AppBody = React.createClass({
               {this.props.children}
             </div>
           </div>
+        </div>
+        <div className="tabs tabs-icon-top">
+          {this.props.tabs.map((tab, i) => {
+              return (
+                <a className="tab-item" key={tab} onClick={this.ionModal.bind(null, tab)}>
+                  <i className="icon ion-star"></i>
+                  {tab}
+                </a>
+              )
+            })
+          }
         </div>
       </div>
     )
---
+++ client/components/IonModal.jsx
@@ -0,0 +1,27 @@
+Backdrop = React.createClass({
+  render() {
+    return (
+      <div className="modal-backdrop active"></div>
+    )
+  }
+})
+
+IonModal = React.createClass({
+  render() {
+    return (
+      <div className="modal-backdrop">
+        <div className="modal-wrapper">
+          <div className="modal">
+            <div className="bar bar-light bar-header">
+              <div className="content overflow-scroll">
+                <div className="padding">
+                  {this.props.children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
